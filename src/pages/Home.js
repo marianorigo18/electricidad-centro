2478b1ab1d72059c9bd858eaaf72d6828c10c9e3
marianorigo18 <marianowagnerrigo@gmail.com>
2022-10-28 19:07:43 -0300

    components images home right now

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,107 @@
-import { Link } from "react-router-dom";
+import styled from "styled-components";
 import { topic } from "../exports/data";
-import "./Page.css";
-
+const LayoutHome = styled.div`
+  max-width: 1000px;
+  width: 100%;
+  box-sizing: border-box;
+  margin: 120px auto 5px auto;
+`;
+const LayoutContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: repeat(10, 500px);
+  justify-content: center;
+`;
+const GridItem = styled.div`
+  padding: 50px;
+  &:nth-child(4n + 2) {
+    background-color: rgba(98, 206, 60, 0.7);
+    padding: 70px;
+    );
+  }
+  &:nth-child(4n + 3) {
+    padding: 70px;
+    background-color: #151515;
+  }
+`;
+const ImageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #ffffff;
+`;
+const Img = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+const InfoContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+const InfoContImages = styled.div`
+  display: flex;
+  background-color: tomato;
+  width: 100%;
+  height: 50%;
+  padding: 5px;
+`;
+const InfoContSingleImage = styled.div`
+  width: 50%;
+  height: 100%;
+  background-color: lightgreen;
+  border: 1px solid black;
+`
+const InfoContTitle = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+background-color: yellow;
+width: 100%;
+height: 50%;
+`
 const Home = () => {
   return (
-    <div className="layout">
-      {topic.map((topic) => (
-        <div className="topic__container">
-          <div className="topic-container__img">
-            <img className="topic__img" src={topic.img} alt={topic.title}></img>
-          </div>
-          <div className="topic__description">
-            <p className="phrase">{topic.phrase}</p>
-            <h2 className="title">{topic.title}</h2>
-            <p className="categories">{topic.categories}</p>
-            <Link to={topic.link} className="topic__link">
-              <button className="topic__button">Ver Mas</button>
-            </Link>
-          </div>
-        </div>
-      ))}
-    </div>
+    <LayoutHome>
+      <LayoutContainer>
+        <GridItem>
+          <InfoContainer>
+            <InfoContImages>
+              <InfoContSingleImage>
+              <Img src={topic[4].img} alt={topic[4].title}></Img>
+              </InfoContSingleImage>
+              <InfoContSingleImage>
+              <Img src={topic[5].img} alt={topic[5].title}></Img>
+              </InfoContSingleImage>
+            </InfoContImages>
+            <InfoContTitle><p>BIDIRECCIONAL</p></InfoContTitle>
+          </InfoContainer>
+        </GridItem>
+        <GridItem>
+          <ImageContainer>
+            <Img src={topic[0].img} alt={topic[0].title}></Img>
+          </ImageContainer>
+        </GridItem>
+        <GridItem>
+          <ImageContainer>
+            <Img src={topic[0].img} alt={topic[0].title}></Img>
+          </ImageContainer>
+        </GridItem>
+        <GridItem>
+          <ImageContainer>hel</ImageContainer>
+        </GridItem>
+        <GridItem>
+          <ImageContainer>hello</ImageContainer>
+        </GridItem>
+        <GridItem>
+          <ImageContainer>
+            <Img src={topic[1].img} alt={topic[0].title}></Img>
+          </ImageContainer>
+        </GridItem>
+      </LayoutContainer>
+    </LayoutHome>
   );
 };
 
